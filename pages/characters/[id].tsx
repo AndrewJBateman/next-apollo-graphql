31b65cfe4d80775cd3fa1a5274aca9ff0b1e12f8
AndrewJBateman <gomezbateman@yahoo.com>
2022-01-08 15:47:18 +0100

    fix: add return types to remove errors

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -3,7 +3,7 @@ import { getDataFromTree } from "@apollo/client/react/ssr";
 import withApollo from "../../lib/withApollo";
 import { useCharacterQuery } from "../../generated";
 
-function SingleCharacterPage({ query }) {
+function SingleCharacterPage({ query }: any): any {
   const id = get(query, "id");
 
   const { data } = useCharacterQuery({
